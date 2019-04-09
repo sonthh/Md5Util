4536c89b733d03ebd4d0e4295eea967066a88ef0
tranhuuhongson <tranhuuhongson@gmail.com>
2019-04-09 22:46:42 +0700

    md5

---
+++ Md5Util.java
@@ -0,0 +1,22 @@
+package demo.util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Util {
+    public static String md5(String str) {
+        MessageDigest md;
+        String result = "";
+        try {
+            md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            BigInteger bi = new BigInteger(1, md.digest());
+
+            result = bi.toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
